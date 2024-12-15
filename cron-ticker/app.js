7f6075f490a47a7ab2ee9a72d117bfde045a278a
Farney Jiménez <farney9@gmail.com>
2024-12-15 15:31:51 -0500

    add new project for DockerFiles

---
+++ cron-ticker/app.js
@@ -0,0 +1,12 @@
+
+
+const cron = require('node-cron');
+
+let times = 0;
+
+cron.schedule('1-59/5 * * * * * *', () => {
+  times++;
+  console.log('Tick every 5 seconds', times);
+});
+
+console.log('Inicio de la tarea programada');
